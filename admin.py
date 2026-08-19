bb9b2d2d6e49fcce965dd1d1e42d745c58ba1e8e
MT <69726672+M-Tri@users.noreply.github.com>
2026-08-19 12:14:52 +0100

    feat: Register models in admin and add login/register templates with forms

--- admin.py
+++ admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Opinion, Label, Article, Image, Supervise
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Opinion)
+admin.site.register(Label)
+admin.site.register(Article)
+admin.site.register(Image)
+admin.site.register(Supervise)
